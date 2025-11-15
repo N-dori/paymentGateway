9800d237625af8ffaca691d74dc9635e98b8dd42
Nadav Dori <dori.nadav@gmail.com>
2025-11-16 00:44:56 +0200

    fix backend to run

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,10 +3,9 @@ import logger from './services/logger.service'
 import cors from 'cors';
 import path from 'path';
 import http from 'http';
+import  setupAsyncLocalStorage from './middlewares/setupAls.middleware'
+import { setUp } from './services/socket.service';
 
-const socketService =require('./services/socket.service')
-
-import Payment, { IPayment } from './models/Payment';
 
 const app = express();
 const server = http.createServer(app);
@@ -25,56 +24,21 @@ if (process.env.NODE_ENV === 'production') {
     app.use(cors(corsOptions))
 }
 
-const authRoutes = require('./api/auth/auth.routes')
-const orderRoutes = require('./api/order/order.routes')
-const userRoutes = require('./api/user/user.routes')
+// const authRoutes = require('./api/auth/auth.routes')
+// const userRoutes = require('./api/user/user.routes')
  
  // routes
-const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
 app.all('*', setupAsyncLocalStorage)
 
-app.use('/api/auth', authRoutes)
-app.use('/api/order', orderRoutes)
-app.use('/api/user', userRoutes)
+// app.use('/api/auth', authRoutes)
+// app.use('/api/user', userRoutes)
 
 app.get('/**', (req:Request, res:Response) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-const port = process.env.PORT || 3030
-// http.listen(port, () => {
-//     logger.info('Server is running on port: ' + port)
-// })
-socketService.setUp(http,corsOptions)
-server.listen(port, () => {
-    logger.info(`Server is running on port http://localhost:${port}`)
-})
-
-//   try {
-//     const { merchant_id, amount } = req.body;
-//     const payment: IPayment = await Payment.create({ merchant_id, amount });
-//     io.emit('payment_created', payment);
-//     res.json(payment);
-//   } catch (err) {
-//     res.status(500).json({ error: 'Failed to create payment' });
-//   }
-// });
-
-// app.post('/api/payments/:id/confirm', async (req, res) => {
-//   try {
-//     const payment: IPayment | null = await Payment.findByIdAndUpdate(
-//       req.params.id,
-//       { status: 'confirmed' },
-//       { new: true }
-//     );
-//     if (!payment) return res.status(404).json({ error: 'Payment not found' });
-//     io.emit('payment_confirmed', payment);
-//     res.json(payment);
-//   } catch (err) {
-//     res.status(500).json({ error: 'Failed to confirm payment' });
-//   }
-// });
-
-// const PORT = process.env.PORT || 4000;
-// connect(process.env.MONGO_URI || 'mongodb://localhost:27017/payment_gateway')
-//   .then(() => server.listen(PORT, () => console.log(`🚀 Backend running on ${PORT}`)));
+const PORT = process.env.PORT || 3030;
+server.listen(PORT, () => {
+  logger.info(`Server is running on port: ${PORT}`);
+  setUp(server, corsOptions);
+});
